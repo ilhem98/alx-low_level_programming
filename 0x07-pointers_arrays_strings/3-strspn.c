be7b509129212f3e80eb1ff247337e1039e53b91
ilhem98 <ilhem@DESKTOP-I4994NJ.localdomain>
2023-08-28 19:34:14 +0100

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,26 @@
-nclude "main.h"
-/**
- *  * _strspn - Entry point
- *   * @s: input
- *    * @accept: input
- *     * Return: Always 0 (Success)
- *      */
-unsigned int _strspn(char *s, char *accept)
+#include "main.h"
+/** _strspn -Entry point
+ * @s: input
+ * @accpet: input
+ * Return: Always 0 (Success)
+ */
+unsigned int_strspn'char *s, char *accept)
 {
-		unsigned int n = 0;
-			int r;
-
-				while (*s)
-						{
-									for (r = 0; accept[r]; r++)
-												{
-																if (*s == accept[r])
-																				{
-													n++;
-													break;												}
-else if (accept[r + 1] == '\0')										return (n);
-													s++;
-}
-return (n);
+	unsigned int n = 0;
+	int r;
+	while (*s)
+	{
+		for (r=0;accept[r];r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else of (accept[r+1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
 }
